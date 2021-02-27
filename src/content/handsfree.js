3c5335d97f1eafb51b7f6ee42d4dbeb5f8dc8ced
ong6 <junxiongong2@gmail.com>
2021-02-27 16:15:05 +0800

    Pause Vids with pinch

--- src/content/handsfree.js
+++ src/content/handsfree.js
@@ -1,48 +1,38 @@
 handsfree = new Handsfree({
-  isClient: true,
-  hands: true,
-  // weboji: true
-})
-handsfree.enablePlugins('browser')
+	isClient: true,
+	hands: true,
+	// weboji: true
+});
+handsfree.enablePlugins("browser");
 
 /**
  * Pinch Click
  */
-handsfree.use('pinchClick', ({hands}) => {
-  if (!hands.multiHandLandmarks) return
+handsfree.use("pinchClick", ({ hands }) => {
+	if (!hands.multiHandLandmarks) return;
 
-  hands.pointer.forEach((pointer, hand) => {
-    if (pointer.isVisible && hands.pinchState[hand][0] === 'start') {
-      const $el = document.elementFromPoint(pointer.x, pointer.y)
-      if ($el) {
-        $el.dispatchEvent(
-          new MouseEvent('click', {
-            bubbles: true,
-            cancelable: true,
-            clientX: pointer.x,
-            clientY: pointer.y
-          })
-        )
-  
-        // Focus
-        if (['INPUT', 'TEXTAREA', 'BUTTON', 'A'].includes($el.nodeName))
-          $el.focus()
-      }
-    }
-  })
-})
+	hands.pointer.forEach((pointer, hand) => {
+		if (pointer.isVisible && hands.pinchState[hand][0] === "start") {
+			document.addEventListener("keydown", function (event) {
+				console.log(event);
+			});
+			var evt = new KeyboardEvent("keydown", { keyCode: 32, which: 32 });
+			document.dispatchEvent(evt);
+		}
+	});
+});
 
 /**
  * Handle messages from background script
  */
-chrome.runtime.onMessage.addListener(function(message) {
-  switch (message.action) {
-    case 'handsfree-data':
-      handsfree.runPlugins(message.data)
-    break
+chrome.runtime.onMessage.addListener(function (message) {
+	switch (message.action) {
+		case "handsfree-data":
+			handsfree.runPlugins(message.data);
+			break;
 
-    case 'handsfree-debug':
-      console.log(message.data)
-    break
-  }
-})
+		case "handsfree-debug":
+			console.log(message.data);
+			break;
+	}
+});
